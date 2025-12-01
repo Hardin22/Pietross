dcec05ba047521cef364c9822bdf972918eb8a4a
Francesco Albano <francesco.albano@studenti.unina.it>
2025-12-01 11:58:30 +0100

    Initial Commit

---
+++ Memories/MemoriesApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemoriesApp.swift
+//  Memories
+//
+//  Created by Francesco Albano on 01/12/25.
+//
+
+import SwiftUI
+
+@main
+struct MemoriesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ MemoriesTests/MemoriesTests.swift
@@ -0,0 +1,17 @@
+//
+//  MemoriesTests.swift
+//  MemoriesTests
+//
+//  Created by Francesco Albano on 01/12/25.
+//
+
+import Testing
+@testable import Memories
+
+struct MemoriesTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
